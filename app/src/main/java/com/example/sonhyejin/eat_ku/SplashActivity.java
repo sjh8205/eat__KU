68b8aca010bb1853ae821d9b1b7a4d3ff2d479d4
sonhyejin <sjh8205@naver.com>
2018-05-28 23:02:44 +0900

    add_splash

---
+++ app/src/main/java/com/example/sonhyejin/eat_ku/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.sonhyejin.eat_ku;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SplashActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
