e50de70d9034f0f4b1f75de377faa36ae3e7e449
sonhyejin <sjh8205@naver.com>
2018-05-29 17:51:47 +0900

    add_home_button_and_forth_page

--- app/src/main/java/com/example/sonhyejin/eat_ku/Fix_ref.java
+++ app/src/main/java/com/example/sonhyejin/eat_ku/Fix_ref.java
@@ -1,7 +1,13 @@
 package com.example.sonhyejin.eat_ku;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 public class Fix_ref extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class Fix_ref extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fix_ref);
+
+        Button home = (Button)findViewById(R.id.home1);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //Fix_ref 페이지로 넘어가기 (fix_Refrigerator : 냉장고 편집)
+                Intent intent = new Intent(Fix_ref.this, Mainmenu.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/sonhyejin/eat_ku/Rec_rec.java
+++ app/src/main/java/com/example/sonhyejin/eat_ku/Rec_rec.java
@@ -1,7 +1,10 @@
 package com.example.sonhyejin.eat_ku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Rec_rec extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Rec_rec extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rec_rec);
+
+        Button home = (Button)findViewById(R.id.home2);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //Fix_ref 페이지로 넘어가기 (fix_Refrigerator : 냉장고 편집)
+                Intent intent = new Intent(Rec_rec.this, Mainmenu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
