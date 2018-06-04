ea4911db6e76cd73323a819a1119337fcf68b36e
LittleBird <dlwldnjs950@naver.com>
2018-06-04 21:21:15 +0900

    일단 info.csv파일만 배열에 넣음!

--- app/src/main/java/com/example/sonhyejin/eat_ku/Rec_rec.java
+++ app/src/main/java/com/example/sonhyejin/eat_ku/Rec_rec.java
@@ -1,10 +1,14 @@
 package com.example.sonhyejin.eat_ku;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.InputStream;
 
 public class Rec_rec extends AppCompatActivity {
 
@@ -22,5 +26,47 @@ public class Rec_rec extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        AssetManager am = getResources().getAssets();
+        InputStream is = null;
+        byte buf[] = new byte[1000000];
+        String text = "";
+
+        try{
+            is = am.open("info.csv");
+
+            if(is.read(buf) > 0){
+                text = new String(buf);
+            }
+
+            String[] indat = new String[539];
+
+            for(int i=0;i<539;i++){ //한줄을 자르고
+                indat =  text.split("\n");
+            }
+
+            String[][] indat1= new String[539][16];
+
+            for(int i=0;i<539;i++){ //각 줄을 또 자르고
+                for(int j=0;j<16;j++) {
+                    indat1[i] = indat[i].split(",");
+                }
+            }
+
+            TextView textView = (TextView) findViewById(R.id.text1);
+            textView.setText(indat1[537][1]);
+
+            is.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        if(is != null){
+            try{
+                is.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
